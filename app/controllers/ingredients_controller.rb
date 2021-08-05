be5ba6728fe2131aa9f1b4f10280114e97356b73
essbee808 <sheena.myb@gmail.com>
2021-08-04 18:01:53 -0700

    Create ingredients controller and views

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,10 @@
+class IngredientsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'ingredients/new'
+
+  get 'ingredients/edit'
+
+  get 'ingredients/index'
+
   resources :recipes 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
