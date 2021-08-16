b134649310fd1fb88224549a3d81de118919501c
essbee808 <sheena.myb@gmail.com>
2021-08-16 14:16:45 -0700

    Create join table and modify forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,32 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update]
+  before_action :get_ingredients, only: [:new, :edit]
+
   def new
     @recipe = Recipe.new
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    recipe = Recipe.new(recipe_params)
 
+    if recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
   end
 
   def edit
-    
+  end
+
+  def update
+    @recipe.update(recipe_params)
+
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :edit
+    end
   end
 
   def show
@@ -22,7 +39,16 @@ class RecipesController < ApplicationController
 
   private
 
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def get_ingredients
+    @ingredients = Ingredient.all
+  end
+
   def recipe_params
     params.require(:recipe).permit(:name, ingredient_ids: [])
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210805003607) do
+ActiveRecord::Schema.define(version: 20210816211337) do
 
   create_table "ingredients", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "recipe_id"
+    t.integer  "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
